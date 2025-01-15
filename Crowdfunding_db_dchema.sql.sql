c199f0a7f58c6a7815f21303b80be8f44bb0bc9e
MavenDylan <129122795+MavenDylan@users.noreply.github.com>
2025-01-15 16:48:37 -0600

    STARTUP_Crowdfunding_db_dchema.sql.sql

--- Crowdfunding_db_dchema.sql.sql
+++ Crowdfunding_db_dchema.sql.sql
@@ -3,57 +3,57 @@
 
 
 CREATE TABLE "campaign" (
-    "cf_id" Int   NOT NULL,
-    "contact_id" Int   NOT NULL,
-    "company_name" VARCHAR   NOT NULL,
-    "description" VARCHAR   NOT NULL,
-    "goal" DECIMAL   NOT NULL,
-    "pledged" DECIMAL   NOT NULL,
-    "outcome" VARCHAR   NOT NULL,
-    "backers_count" Int   NOT NULL,
-    "country" VARCHAR   NOT NULL,
-    "currency" VARCHAR   NOT NULL,
-    "launched_date" DATE   NOT NULL,
-    "end_date" DATE   NOT NULL,
-    "category_id" VARCHAR   NOT NULL,
-    "subcategory_id" VARCHAR   NOT NULL,
+    "cf_id" INTEGER NOT NULL,
+    "contact_id" INTEGER NOT NULL,
+    "company_name" VARCHAR(100) NOT NULL,
+    "description" TEXT NOT NULL,
+    "goal" DECIMAL(10, 2) NOT NULL,
+    "pledged" DECIMAL(10, 2) NOT NULL,
+    "outcome" VARCHAR(50) NOT NULL,
+    "backers_count" INTEGER NOT NULL,
+    "country" VARCHAR(50) NOT NULL,
+    "currency" VARCHAR(10) NOT NULL,
+    "launched_date" DATE NOT NULL,
+    "end_date" DATE NOT NULL,
+    "category_id" VARCHAR(10) NOT NULL,
+    "subcategory_id" VARCHAR(10) NOT NULL,
     CONSTRAINT "pk_campaign" PRIMARY KEY (
         "cf_id"
      )
 );
 
 CREATE TABLE "category" (
-    "category_id" VARCHAR   NOT NULL,
-    "category" VARCHAR   NOT NULL,
+    "category_id" VARCHAR(10) NOT NULL,
+    "category_name" VARCHAR(50) NOT NULL,
     CONSTRAINT "pk_category" PRIMARY KEY (
         "category_id"
      )
 );
 
 CREATE TABLE "contacts" (
-    "contact_id" Int   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "email" VARCHAR   NOT NULL,
+    "contact_id" INTEGER NOT NULL,
+    "first_name" VARCHAR(50) NOT NULL,
+    "last_name" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
     CONSTRAINT "pk_contacts" PRIMARY KEY (
         "contact_id"
      )
 );
 
 CREATE TABLE "subcategory" (
-    "subcategory_id" VARCHAR   NOT NULL,
-    "subcategory" VARCHAR   NOT NULL,
+    "subcategory_id" VARCHAR(10) NOT NULL,
+    "subcategory_name" VARCHAR(50) NOT NULL,
     CONSTRAINT "pk_subcategory" PRIMARY KEY (
         "subcategory_id"
      )
 );
 
-ALTER TABLE "category" ADD CONSTRAINT "fk_category_category_id" FOREIGN KEY("category_id")
-REFERENCES "campaign" ("category_id");
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
 
-ALTER TABLE "contacts" ADD CONSTRAINT "fk_contacts_contact_id" FOREIGN KEY("contact_id")
-REFERENCES "campaign" ("contact_id");
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
 
-ALTER TABLE "subcategory" ADD CONSTRAINT "fk_subcategory_subcategory_id" FOREIGN KEY("subcategory_id")
-REFERENCES "campaign" ("subcategory_id");
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
 
